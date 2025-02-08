97d7213d7df768e55a1f1797cf89b88b674bed08
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-02-08 11:05:27 +0500

    Commited

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,10 +6,6 @@ async def set_default_commands(dp):
         [
             types.BotCommand("start", "Botni ishga tushurish"),
             types.BotCommand("admin", "Admin panel"),
-            types.BotCommand("ro", "Foydalanuvchini Read Only (RO) rejimga o'tkazish"),
-            types.BotCommand("unro", "RO rejimdan chiqarish"),
-            types.BotCommand("ban", "Ban"),
-            types.BotCommand("unban", "Bandan chiqarish"),
             types.BotCommand("/bekor", "Jarayonni bekor qilish")
         ]
     )
