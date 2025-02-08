176272f213c9a5f8afe837ba57b39881952d8c42
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-02-08 10:56:39 +0500

    Commited

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -3,17 +3,17 @@ import traceback
 
 from aiogram import Dispatcher
 
-from data.config import ADMIN_GROUP_ID
+from data.config import ADMIN_GROUP_ID, ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
     try:
-        await dp.bot.send_message(ADMIN_GROUP_ID, "Bot ishga tushdi")
+        await dp.bot.send_message(ADMINS[0], "Bot ishga tushdi")
 
     except Exception as err:
         logging.error(f"Xatolik: {err}")
         logging.error("Traceback:\n" + traceback.format_exc())
         error_text = f"Xatolik:\n{err}\n\nTraceback:\n" + traceback.format_exc()
         await dp.bot.send_message(
-            chat_id=ADMIN_GROUP_ID, text=error_text
+            chat_id=ADMINS[0], text=error_text
         )
