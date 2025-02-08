c6f0f8331c407c61062b4a8dd22ef70faad5e6d3
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-02-08 17:08:11 +0500

    Commited

--- keyboards/inline/admin.py
+++ keyboards/inline/admin.py
@@ -4,15 +4,17 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def chapters_ibutton():
     markup = InlineKeyboardMarkup(row_width=1)
     markup.add(
-        InlineKeyboardButton(text="Kimyo (DTM | PDF)", callback_data="dtm_chemistry_pdf"),
-        InlineKeyboardButton(text="Kimyo (DTM | Image)", callback_data="dtm_chemistry_image"),
-        InlineKeyboardButton(text="Kimyo (Sertifikat | PDF)", callback_data="sert_chemistry_pdf"),
-        InlineKeyboardButton(text="Kimyo (Sertifikat | Image)", callback_data="sert_chemistry_image"),
-        InlineKeyboardButton(text="Biologiya (DTM | PDF", callback_data="dtm_biology_pdf"),
-        InlineKeyboardButton(text="Biologiya (DTM | Image)", callback_data="dtm_biology_image"),
-        InlineKeyboardButton(text="Biologiya (Sertifikat | PDF)", callback_data="sert_biology_pdf"),
-        InlineKeyboardButton(text="Biologiya (Sertifikat | Image)", callback_data="sert_biology_image"),
+        InlineKeyboardButton(text="Kimyo (DTM | PDF)", callback_data="ch:dtm_chemistry_pdf"),
+        InlineKeyboardButton(text="Kimyo (DTM | Image)", callback_data="ch:dtm_chemistry_img"),
+        InlineKeyboardButton(text="Kimyo (Sertifikat | PDF)", callback_data="ch:sert_chemistry_pdf"),
+        InlineKeyboardButton(text="Kimyo (Sertifikat | Image)", callback_data="ch:sert_chemistry_img"),
+        InlineKeyboardButton(text="Biologiya (DTM | PDF", callback_data="ch:dtm_biology_pdf"),
+        InlineKeyboardButton(text="Biologiya (DTM | Image)", callback_data="ch:dtm_biology_img"),
+        InlineKeyboardButton(text="Biologiya (Sertifikat | PDF)", callback_data="ch:sert_biology_pdf"),
+        InlineKeyboardButton(text="Biologiya (Sertifikat | Image)", callback_data="ch:sert_biology_img"),
+        InlineKeyboardButton(text="Ortga", callback_data="back_to_admin")
     )
+    return markup
 
 
 def delete_pdf_ibutton(id_):
